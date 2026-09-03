b7d06ecd037c59f0a965e44c39c6c093379befe3
XP <x@hallofthegods.com>
2026-09-03 04:27:02 -0700

    add settings link

--- xophz-compass-bugnet.php
+++ xophz-compass-bugnet.php
@@ -99,3 +99,13 @@ function run_xophz_compass_bugnet() {
   
 }
 add_action( 'plugins_loaded', 'run_xophz_compass_bugnet' );
+
+function xophz_compass_bugnet_action_links( $links ) {
+  $settings_link = '<a href="edit.php?post_type=compass_bug&page=bugnet-github-settings">' . __( 'Settings', 'xophz-compass-bugnet' ) . '</a>';
+  $new_links = array( 'settings' => $settings_link );
+  foreach ( $links as $key => $value ) {
+    $new_links[ $key ] = $value;
+  }
+  return $new_links;
+}
+add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'xophz_compass_bugnet_action_links' );
